8e02d6653d61e3de5b4775615a9900af1eebb3ff
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-02-19 15:11:44 +0100

    first commit

---
+++ SwifUI-handbook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwifUI-handbook
+//
+//  Created by Amjad Oudeh on 19.02.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwifUI-handbook/SwifUI_handbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwifUI_handbookApp.swift
+//  SwifUI-handbook
+//
+//  Created by Amjad Oudeh on 19.02.22.
+//
+
+import SwiftUI
+
+@main
+struct SwifUI_handbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
