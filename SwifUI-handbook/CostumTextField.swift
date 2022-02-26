d6bcecf827dbbf68f26b32c2704d76fc2d18ec37
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-02-26 17:56:58 +0100

    Input Field using 2 ways of animation

--- SwifUI-handbook/CostumTextField.swift
+++ SwifUI-handbook/CostumTextField.swift
@@ -8,22 +8,47 @@
 import SwiftUI
 
 struct CustomTextField: View {
-     @State var name = ""
+    @State var name1 = ""
+    @State var name = ""
     var body: some View {
         Form {
-            Section (header: Text("")) {
-                VStack (alignment: .leading) {
-                    if !name.isEmpty {
-                        Text("First Name")
-                            .font(.caption)
-                            .foregroundColor(.secondary)
+            
+            VStack {
+                Section {
+                    VStack (alignment: .leading) {
+                        if !name1.isEmpty {
+                            Text("First Name")
+                                .font(.caption)
+                                .foregroundColor(.secondary)
+                        }
+                        TextField("First Name", text: $name1)
                     }
-                    TextField("First Name", text: $name)
+                }
+                .animation(.default)
+
+                Section {
+                    
+                    VStack(alignment: .leading) {
+                        ZStack (alignment: .leading) {
+                            Text("Last Name")
+                                .foregroundColor(.secondary)
+                                .offset(x: 0, y: name.isEmpty ? 0 : -25)
+                                .scaleEffect(name.isEmpty ? 1 : 0.8, anchor: .leading)
+                            TextField("",text: $name)
+                        }
+                        .padding(.top, 20)
+                        .animation(.default)
+                    }
+                    
                 }
             }
         }
+        
     }
 }
+
+
+
 
 struct TextField_Previews: PreviewProvider {
     static var previews: some View {
