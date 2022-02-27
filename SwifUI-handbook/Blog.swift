cfc43e7c856fc787a3cb2643b4b8f869610ffbe3
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-02-27 10:44:42 +0100

    Edited into.plist

--- SwifUI-handbook/Blog.swift
+++ SwifUI-handbook/Blog.swift
@@ -13,7 +13,8 @@ struct Blog: View {
             Color.orange.ignoresSafeArea(.all)
             
             Text("We write a lot of unit tests while working on the SoundCloud iOS application. Unit tests are obviously great. They’re short, they’re (hopefully) easy to read, and they give us confidence that the code we ship works as expected. But unit tests — as their name suggests — only tend to cover a single unit of code, most often a function or class. So how do we catch the bugs that exist in the interactions between classes — bugs like memory leaks?")
-                .font(.system(size: 30, weight: .semibold, design: .rounded))
+                .font(.custom("Roboto-Italic", size: 25))
+               // .font(.system(size: 30, weight: .semibold, design: .rounded))
                 .padding()
                 .multilineTextAlignment(.leading)
                 .lineSpacing(.pi)
