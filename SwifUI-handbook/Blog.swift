46c6c02ca430db2e56f01623b81549197233a717
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-03-01 13:17:52 +0100

    Excreted the view into a method!

--- SwifUI-handbook/Blog.swift
+++ SwifUI-handbook/Blog.swift
@@ -8,16 +8,20 @@
 import SwiftUI
 
 struct Blog: View {
+    fileprivate func editText() -> Text {
+        return Text("We write a lot of unit tests while working on the SoundCloud iOS application. Unit tests are obviously great. They’re short, they’re (hopefully) easy to read, and they give us confidence that the code we ship works as expected. But unit tests — as their name suggests — only tend to cover a single unit of code, most often a function or class. So how do we catch the bugs that exist in the interactions between classes — bugs like memory leaks?").font(.custom("Roboto-Italic", size: 25))
+        // .font(.system(size: 30, weight: .semibold, design: .rounded))
+         .padding()
+         .multilineTextAlignment(.leading)
+         .lineSpacing(.pi) as! Text
+    }
+    
     var body: some View {
         ZStack {
             Color.orange.ignoresSafeArea(.all)
             
-            Text("We write a lot of unit tests while working on the SoundCloud iOS application. Unit tests are obviously great. They’re short, they’re (hopefully) easy to read, and they give us confidence that the code we ship works as expected. But unit tests — as their name suggests — only tend to cover a single unit of code, most often a function or class. So how do we catch the bugs that exist in the interactions between classes — bugs like memory leaks?")
-                .font(.custom("Roboto-Italic", size: 25))
-               // .font(.system(size: 30, weight: .semibold, design: .rounded))
-                .padding()
-                .multilineTextAlignment(.leading)
-                .lineSpacing(.pi)
+            editText()
+                
         }
     }
 }
